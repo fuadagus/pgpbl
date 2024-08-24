a0833c0c28a528b69a8c72a236ec8f7e5e69466d
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-08-24 22:12:27 +0700

    task component

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task-card',
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-card.component.scss'],
 })
 export class TaskCardComponent  implements OnInit {
+  @Input() hasHeader:boolean = false;
+  @Input() hasFooter:boolean = false;
 
+  @Input() createdAt:string = "null";
+  @Input() updatedAt:string = "null";
+  @Input() status:string= "null";
+  @Input() notes:string= "Tidak ada catatan";
+  @Input() value:string= "Belum dinilai";
   constructor() { }
 
   ngOnInit() {}
