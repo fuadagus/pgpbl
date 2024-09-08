8c70c5b3a617083fb7ef756ad7013b05c1d6b25b
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-09-09 01:22:26 +0700

    implement conditional logic to register button, just valid form only can use it

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -27,9 +27,11 @@ export class RegisterPage implements OnInit {
   }
 
   register() {
+    if (this.registerForm.getForm().valid) {
     this.router.navigate(['home']);
 
   }
+}
 
   private createForm() {
     this.registerForm = new RegisterPageForm(this.formBuilder);
