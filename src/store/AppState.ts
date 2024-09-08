437eb5953ff67aa7b9be633089c64ccf066a4d6a
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-09-09 01:47:04 +0700

    add register state to AppState

--- src/store/AppState.ts
+++ src/store/AppState.ts
@@ -1,7 +1,9 @@
 import { LoadingState } from "./loading/LoadingState";
 import { LoginState } from "./login/LoginState";
+import { RegisterState } from "./register/Register.State";
 
 export interface AppState{
     loading: LoadingState;
     login: LoginState;
+    register: RegisterState;
 }
