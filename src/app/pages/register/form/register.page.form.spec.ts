9e061eee42a1af325f0208a83c84dd848cd6e134
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-09-08 22:34:33 +0700

    add TDD for register form

---
+++ src/app/pages/register/form/register.page.form.spec.ts
@@ -0,0 +1,46 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { RegisterPage } from './register.page';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterPage } from '../register.page';
+import { RegisterPageModule } from '../register.module';
+// import { RegisterPageModule } from './register.module';
+
+describe('RegisterPage', () => {
+  let component: RegisterPage;
+  let fixture: ComponentFixture<RegisterPage>;
+  let router: Router;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RegisterPage ],
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        ReactiveFormsModule,
+        RegisterPageModule
+      ]
+    })
+
+    fixture = TestBed.createComponent(RegisterPage);
+    router = TestBed.get(Router);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create register form on page init', () => {
+    fixture.detectChanges();
+
+    expect(component.register).not.toBeUndefined();
+  })
+
+  it('should go to home page on register', () => {
+    spyOn(router, 'navigate');
+
+    component.register();
+    
+
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
+  });
+});
