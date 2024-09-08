a85ef4319577d552c19901f027bb431cd3f7ded6
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-09-08 22:03:45 +0700

    implement guard as it is

--- src/app/guards/auth/auth-guard.service.ts
+++ src/app/guards/auth/auth-guard.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, switchMap, take } from 'rxjs';
+import { CanLoad, Router } from '@angular/router';
+import { AppState } from 'src/store/AppState';
+import { Store } from '@ngrx/store';
+import { loginSuccess } from 'src/store/login/login.actions';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuard implements CanLoad {
 
-  constructor() { }
-  canLoad(): Observable<boolean> { 
+  constructor(private store: Store<AppState>, private router:Router) { }
 
-    return of(false);
+  canLoad(): Observable<boolean> {
+    return this.store.select('login').pipe(
+      take(1),
+      switchMap(loginState => {
+        if (loginState.isLoggedIn){
+          return of(true);
+        }
+        this.router.navigateByUrl('login');
+        return of(false);
+      })
+    );
   }
-
 }
